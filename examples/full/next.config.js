b5433d9d2b3dddb66fdbfceb2f75a2c62a6cf977
Paul <paul@theapplegates.com>
2022-03-23 19:17:43 -0400

    first commit

---
+++ examples/full/next.config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  staticPageGenerationTimeout: 300,
+  images: {
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com'
+    ],
+    formats: ['image/avif', 'image/webp']
+  }
+})
---
+++ examples/minimal/next.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  staticPageGenerationTimeout: 300,
+  images: {
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com'
+    ],
+    formats: ['image/avif', 'image/webp']
+  }
+}
